25180cf820b9a3ccd71ded84fe16a52c9f7dfbe7
Sneha Davari <SDavari@eagle.org>
2022-09-02 11:07:47 +0530

    more about core

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -9669,13 +9669,24 @@ const github = __nccwpck_require__(6738);
 
 try{
 // throw new Error("some error message");
+core.debug('Debug message');
+core.warning('Warning message');
+core.error('Error message');
+
 const name = core.getInput('who-to-greet');
+core.setSecret(name);
 console.log(`Hello ${name}`);
 
 const time = new Date();
 core.setOutput("time", time.toTimeString());
 
+core.startGroup("Logging github object");
 console.log(JSON.stringify(github, null, "\t"));
+core.endGroup();
+
+core.exprotVariable("Hello", "hello");
+
+
 
 }catch(error){
   core.setFailed(error.message);
