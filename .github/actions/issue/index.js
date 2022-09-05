87814435fa4e16fbe0accdde898430674dfbd98d
Sneha Davari <SDavari@eagle.org>
2022-09-05 18:31:34 +0530

    issue using own action

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -8,7 +8,7 @@ async function run() {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    const octokit = new octokit(token);
+    const octokit = new github.GitHub(token);
 
     const response = await octokit.issues.create({
       // owner: github.context.repo.owner,
